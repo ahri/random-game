e130dd1d8fee8abec6ffa334d1236327f308ff43
Adam Piper <adam@ahri.net>
2012-07-30 13:12:28 +0100

    Strip tabs and debug data.

--- random-game.py
+++ random-game.py
@@ -17,28 +17,28 @@ apps = config['InstallConfigStore']['Software']['Valve']['Steam']['apps']
 
 installed_apps = []
 for app in apps:
-	# not interested in uninstalled apps
-	if apps[app].get('HasAllLocalContent', "0") != "1":
-		continue
+    # not interested in uninstalled apps
+    if apps[app].get('HasAllLocalContent', "0") != "1":
+        continue
 
-	idir = apps[app].get('InstallDir', None)
+    idir = apps[app].get('InstallDir', None)
 
-	# not interested in apps that have no installed dir
-	if idir is None:
-		continue
+    # not interested in apps that have no installed dir
+    if idir is None:
+        continue
 
-	# not interested in test apps
-	if 'valvetestapp' in idir:
-		continue
+    # not interested in test apps
+    if 'valvetestapp' in idir:
+        continue
 
-	# stuff in user dirs is usually dlc, expansions, etc.
-	if '\\common\\' not in idir:
-		continue
+    # stuff in user dirs is usually dlc, expansions, etc.
+    if '\\common\\' not in idir:
+        continue
 
-	installed_apps.append(app)
+    installed_apps.append(app)
 
-for app in installed_apps:
-	print app, apps[app]['InstallDir']
+#for app in installed_apps:
+#    print app, apps[app]['InstallDir']
 
 selected = installed_apps[randint(0, len(installed_apps)-1)]
 print "Running:", selected
