c73a3c63d9800868f5ff722593ca521c2ab31858
Adam Piper <adam@ahri.net>
2012-08-02 22:38:40 +0100

    Get the Steam install path from the Windows registry.

--- random-game.py
+++ random-game.py
@@ -2,8 +2,26 @@ import re
 import json
 from os import startfile
 from random import randint
+from _winreg import ConnectRegistry, OpenKey, CloseKey, EnumValue, HKEY_LOCAL_MACHINE
 
-config = ''.join(open(r'd:\steam\config\config.vdf').readlines())
+
+reg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+steam_reg = OpenKey(reg, r"Software\Valve\Steam")
+steam_install_path = None
+i = 0
+while True:
+    k, v, _ = EnumValue(steam_reg, i)
+    
+    if k == 'InstallPath':
+        steam_install_path = v
+        break
+
+    i += 1
+
+steam_cfg = r'%s\config\config.vdf' % steam_install_path
+
+
+config = ''.join(open(steam_cfg).readlines())
 config = config.replace('{', ':{')
 config = config.replace('}', '},')
 config = re.sub(r'"\t+"', '":"', config)
@@ -13,7 +31,9 @@ config = "{%s" % config
 config = re.sub(r',\n$', '}', config)
 config = json.loads(config)
 
+
 apps = config['InstallConfigStore']['Software']['Valve']['Steam']['apps']
+
 
 installed_apps = []
 for app in apps:
@@ -36,9 +56,6 @@ for app in apps:
         continue
 
     installed_apps.append(app)
-
-#for app in installed_apps:
-#    print app, apps[app]['InstallDir']
 
 selected = installed_apps[randint(0, len(installed_apps)-1)]
 print "Running:", selected
